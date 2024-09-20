d3f507768b6f96915d438088bc6dfbb474d466dc
shun91 <shun9167@gmail.com>
2024-09-20 11:52:37 +0900

    fetch /api/todo

--- app.vue
+++ app.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+
+    <table v-if="data" style="border: solid">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>todo</th>
+        </tr>
+      </thead>
+      <tr v-for="todo in data.todos">
+        <td>{{ todo.id }}</td>
+        <td>{{ todo.todo }}</td>
+      </tr>
+    </table>
+    <template v-else>
+      <p>Loading...</p>
+    </template>
+
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const { data } = await useLazyFetch<any>("/api/todos", { server: false });
+</script>
+
+<style scoped>
+th,
+td {
+  border: solid;
+}
+</style>
